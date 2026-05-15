a0c44ff1318b4b4b2dddc6c89e178baf9d79902d
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-05-16 00:50:10 +0530

    changes made

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://dildar-client.onrender.com/api', // change this with hosted link later
+  baseURL: 'https://dildar-client.onrender.com', // change this with hosted link later
 });
 
 export default API;
