0ff80d5b75d96d3f14cbe2fb27f3c983569cc01c
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-05-18 02:34:16 +0530

    Changes made

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,21 +1,9 @@
 const errorHandler = (err, req, res, next) => {
+  console.log(err.stack);
 
-  const statusCode =
-    res.statusCode && res.statusCode !== 200
-      ? res.statusCode
-      : 500;
-
-  res.status(statusCode).json({
-    success: false,
-    message: err.message || "Server Error",
-    stack:
-      process.env.NODE_ENV === "production"
-        ? null
-        : err.stack,
+  res.status(err.statusCode || 500).json({
+    message: err.message || 'Server Error',
   });
-
 };
 
-module.exports = {
-  errorHandler,
-};
+module.exports = errorHandler;
