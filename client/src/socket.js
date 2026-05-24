7f2301c7d85c017f90d647f423fe5a0d7fe261de
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-05-24 20:41:51 +0530

    updates

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,14 +1,14 @@
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
-// const socket = io("https://dildar.onrender.com");
-
-// export default socket;
-
-import { io } from 'socket.io-client';
-
-const socket = io('http://localhost:5000', {
-  transports: ['websocket'],
-  autoConnect: true,
-});
+const socket = io("https://dildar.onrender.com");
 
 export default socket;
+
+// import { io } from 'socket.io-client';
+
+// const socket = io('http://localhost:5000', {
+//   transports: ['websocket'],
+//   autoConnect: true,
+// });
+
+// export default socket;
